50d9ac00fdaedad6702ccad2e9dd1c7c769bee60
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-04-15 17:28:34 +0200

    Configure User model and set adjustments on views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :username, :length => {:minimum => 3, :maximum => 29}, :uniqueness => true
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
          
   # Setup accessible (or protected) attributes for your model
