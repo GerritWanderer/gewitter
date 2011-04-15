72acda322cc4ff8ec65bb8f183ef806440841ee3
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-04-15 16:14:15 +0200

    Configure user model and change views from *.erb to *.haml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
+  
+  validates :username, :length => {:minimum => 3, :maximum => 29}, :uniqueness => true
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :username, :firstname, :lastname, :password, :password_confirmation, :remember_me
 end
