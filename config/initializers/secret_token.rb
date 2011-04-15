1abfc7be24aff59d80b4152b246baec4e449256a
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-04-15 14:29:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gewitter::Application.config.secret_token = '3ed0bacc5decb6f4edb5a0acb2c7d461d879d2e99e7117b553c46b0f4e2dde3861bebe8af709934ae7b63be0f5478fe407d05864e64d1b7ebd1ca6dae5fd7842'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gewitter::Application.config.session_store :cookie_store, :key => '_gewitter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gewitter::Application.config.session_store :active_record_store
