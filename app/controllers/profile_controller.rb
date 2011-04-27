a9fe95fdfb128e4bce0b72ba9419f863081004e0
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-04-27 16:34:24 +0200

    Add comments

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,22 +6,25 @@ class ProfileController < ApplicationController
 
   def show
     @message = Message.new if user_signed_in?
+    
+    # if no user selected and you are logged in, show private profile page
     if params[:username].nil? && user_signed_in?
       @user = current_user
       subscriptions = Subscription.find_all_by_user_id([@user.id], :select => :profile).map(&:profile)
       @messages = Message.find_all_by_user_id(subscriptions.push(@user.id)) + Mention.find_all_by_user_id(@user.id).map(&:message)
       @messages = @messages.sort_by { |m| m["created_at"] }.reverse
-    else
+    else # Try to find selected User, else redirect to root
       begin
         @user = User.find_by_username!(params[:username])
         @messages = @user.messages
       rescue
-        redirect_to profiles_path
+        redirect_to profiles_path, :notice => 'Sorry, User does not exist'
       end
     end
   end
 
   def subscribe
+    # find user and create subscription record
     begin
       subscribed_profile = User.find_by_username!(params[:username])
     rescue
@@ -31,18 +34,20 @@ class ProfileController < ApplicationController
     authorize! :create, subscription
     
     if subscription.save
-      redirect_to profile_path, :notice => "You have successfully subscribed"
+      flash[:notice] = "You have successfully subscribed"
     else
-      redirect_to profile_path, :notice => "Error occurred"
+      flash[:alert] = "Error occurred while subscribing the User"
     end
+    redirect_to profile_path
   end
   
   def unsubscribe
+    # find user and delete subscription record
     begin
       subscribed_profile = User.find_by_username!(params[:username])
       subscription = Subscription.where(:user_id => current_user.id, :profile => subscribed_profile.id).first
     rescue
-      redirect_to profiles_path, :alert => 'Subscription was not found - please try again later'
+      redirect_to profiles_path, :alert => 'Subscription was not found'
     end
     authorize! :destroy, subscription
     redirect_to profile_path, :notice => 'Subscription was successfully deleted' if subscription.destroy
--- app/models/message.rb
+++ app/models/message.rb
@@ -8,6 +8,8 @@ class Message < ActiveRecord::Base
   
   private
   def create_mentions
+    # Split words of the message as array and compare it with a list of existing usernames
+    # extract duplicates between both arrays into a seperate array and write Mention records
     mention_usernames = User.all(:select => :username).map(&:username) & self.text.split(/\W/)
     mention_usernames.each do |mention_username|
       Mention.create!(:user_id => User.find_by_username(mention_username).id, :message_id => self.id)
